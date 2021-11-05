d7e92a70b3ca205d6329eb842d38b2fc6d63d008
elduha <79367834+elduha@users.noreply.github.com>
2021-11-05 19:40:08 +0600

    Delete flaskblog directory

--- flaskblog/forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-
-
-class CheckForm(FlaskForm):
-    crypto_name = StringField('Crypto')
-    check = SubmitField('Check')
